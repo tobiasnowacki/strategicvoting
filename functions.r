cea9b9a35795c5325551c0acee5d1e07568b2960
tobiasnowacki <toby.nowacki@gmail.com>
2018-10-30 15:24:03 -0700

    first_func
    Adding first files

---
+++ functions.r
@@ -0,0 +1,19 @@
+##################################################
+## Project: Strategic Voting in RCV
+## Script purpose: Useful functions for project
+## Date: 30/10/2018
+## Author:
+##################################################
+
+# Calculate Optimal Votes
+## Takes input: utility dataframe; matrix of pivotal probabilities.
+## Gives output: matrix of expected utilities of each vote
+
+## combine dataframes such that each row has: (u_A_ABC, u_B_ABC, u_C_ABC, u_A_ACB, ...)
+
+opt_vote <- function(util_df, piv_prob_df, type = "rcv"){
+
+
+}
+
+# Build toy example
