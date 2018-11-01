3a4ec666b4ff3ee2a72b9364f6b78bdae4b27153
tobiasnowacki <toby.nowacki@gmail.com>
2018-10-31 22:40:42 -0700

    SV proportions
    wrote functions; wrote script to plot distribution of SV by constituency (NSW) and levels of s

--- code/replication_aus.R
+++ code/replication_aus.R
@@ -7,13 +7,68 @@
 ##################################################
 
 # Set WD etc.
+library(here)
 
+av_piv_path <- here("code/utils/av_pivotal_probs_analytical_general_v2.r")
+source(av_piv_path)
+functions <- here("code/utils/functions.r")
+source(functions)
+
+library(ggplot2)
+library(reshape2)
 
 # Import AES and Ballot data
+# Note that these are normalised utilities. To obtain like-dislike scores I will need to re-run the original script.
+aes_utils <- read.csv(here("data", "australia", "AES_utility.csv"))[, -1]
+aes_utils <- aes_utils[, c("GRN", "LIB", "LAB")] #common ordering
+
+nsw <- read.csv(here("data/australia/nsw_ballots.csv"))[, -1]
+resampling <- read.csv(here("data/australia/nsw_resampling.csv"))[, -1]
+
+# Create DF with ballotprofiles
+const_bp <- data.frame(district = nsw$District,
+                       AB = nsw$`GRN.LIB` + nsw$`GRN.LIB.LAB`,
+                       AC = nsw$`GRN.LAB` + nsw$`GRN.LAB.LIB`,
+                       BA = nsw$`LIB.GRN` + nsw$`LIB.GRN.LAB`,
+                       BC = nsw$`LIB.LAB` + nsw$`LIB.LAB.GRN`,
+                       CA = nsw$`LAB.GRN` + nsw$`LAB.GRN.LIB`,
+                       CB = nsw$`LAB.LIB` + nsw$`LAB.LIB.GRN`,
+                       A = nsw$GRN,
+                       B = nsw$LIB,
+                       C = nsw$LAB)
+
+# For now, let's not use truncated ballots:
+const_bp_no_trunc <- const_bp
+const_bp_no_trunc[, 8:10] <- 0
+const_bp_no_trunc[, 2:10] <- t(apply(const_bp_no_trunc[, 2:10], 1, function(x) x / sum(x)))
 
 
-# Calculate expected utility for every voter under each of the constituency scenarios
+# Return data.frame with levels of strat voting by s and constituency.
 
-# Check proportion of voters whose optimal vote is strategic.
+# Set levels of s at which to evaluate.
+s_list <- as.list(seq(from = 10, to = 130, by = 10))
+
+# Run loop over all constituencies.
+const_opt_dist <- list()
+for(i in 1:nrow(const_bp_no_trunc)){
+	print(i)
+	prop <- return_sv_prop(const_bp_no_trunc[i, 2:10], aes_utils[, 1:3], s_list)
+	prop[, 1:3] <- as.data.frame(t(apply(prop[, 1:3], 1, function(x) x / sum(x))))
+	prop$const <- const_bp_no_trunc[i, 1]
+	const_opt_dist[[i]] <- prop
+}
+
+const_opt_dist_df <- do.call(rbind, const_opt_dist)
+const_opt_dist_df <- melt(const_opt_dist_df, id.vars = c("const", "s"))
 
 # Plot.
+
+hist(const_opt_dist_df$value[const_opt_dist_df$variable == "third" ])
+
+ggplot(const_opt_dist_df, aes(x = s, y = value)) +
+	geom_line(aes(colour = variable, group = interaction(const, variable)), alpha = 0.3) +
+	labs(x = "Information (s)", 
+		y = "Proportion of voters in AES casting ballot type",
+		colour = "Sincere pref. as first on ballot") +
+	theme_bw() +
+	theme(legend.position = "bottom")
--- code/utils/functions.r
+++ code/utils/functions.r
@@ -5,11 +5,6 @@
 ## Author:
 ##################################################
 
-##################################################
-# (Delete later, only necessary for toy example) #
-setwd("~/github/strategicvoting/utils")
-source("av_pivotal_probs_analytical_general_v2.r")
-##################################################
 
 opt_vote <- function(utility_df, p_list, type = "rcv"){
 # Calculate Optimal Votes
@@ -51,63 +46,137 @@ opt_vote <- function(utility_df, p_list, type = "rcv"){
 	test[, c(F, F, T)]
 	cond_utils_sum_event <- cond_utils_sum * p_list
 	vote_utils <- colSums(cond_utils_sum_event)
+	vote_utils <- matrix(vote_utils, ncol = 6, byrow = T)
 	return(vote_utils)
+}
+
+return_sv_prop <- function(v_vec, util_df, s_breaks){
+	# Function that takes a ballot profile, a dataframe of utilities, and a list of s values
+	#	Returns: a data.frame with voters by vote type for levels of s
+
+	v_vec <- as.numeric(v_vec)
+	sin_vec <- apply(util_df, 1, sin_vote_scalar)
+	p_list <- lapply(s_breaks, function(x) av.pivotal.event.probs.general(v_vec, rep(x, 4)))
+	eu_list <- lapply(p_list, function(x) opt_vote(util_df, x, type = "rcv"))
+	opt_vec_list <- lapply(eu_list, function(x) opt_vote_scalar(x))
+	opt_vote_prop <- lapply(opt_vec_list, function(x) sum_opt_votes(sin_vec, x))
+
+	prop_df <- do.call(rbind, opt_vote_prop)
+	prop_df <- as.data.frame(prop_df)
+	prop_df$s <- unlist(s_breaks)
+	names(prop_df)[1:3] <- c("first", "second", "third")
+	return(prop_df)
+
+}
+
+# Function: Take EU df and calculate optimal vote
+
+opt_vote_scalar <- function(eu_df){
+	apply(eu_df, 1, function(x) which(x == max(x))) 
+}
+
+sin_vote_scalar <- function(util){
+  # Function that gets the "number" of pref. ordering type
+  stopifnot(length(util) == 3)
+  max <- which(util == max(util))
+  min <- which(util == min(util))
+  
+  if (max == 1){
+    if (min == 3){
+      out <- 1
+    }
+    if (min == 2){
+      out <- 2
+    }
+  }
+  if (max == 2){
+    if (min == 3){
+      out <- 3
+    }
+    if (min == 1){
+      out <- 4
+    }
+  }
+  if (max == 3){
+    if (min == 2){
+      out <- 5
+    }
+    if (min == 1){
+      out <- 6
+    }
+  }
+  return(out)
+}
+
+sum_opt_votes <- function(sin_vec, opt_vec){
+	sin_vec <- factor(sin_vec, levels = 1:6)
+	opt_vec <- factor(opt_vec, levels = 1:6)
+	tab <- table(sin_vec, opt_vec)
+	# print(tab)
+	out <- rbind(c(tab[1, 1], tab[1, 3], tab[1, 5]),
+				 c(tab[2, 2], tab[2, 5], tab[2, 3]),
+				 c(tab[3, 3], tab[3, 1], tab[3, 6]),
+				 c(tab[4, 4], tab[4, 6], tab[4, 1]),
+				 c(tab[5, 5], tab[5, 2], tab[5, 4]),
+				 c(tab[6, 6], tab[6, 4], tab[6, 2]))
+	return(colSums(out))
+
 }
 
 # Andy's function(s) (for reference):
 
-outcome_mat_from_utility = function(utility, piv.events = c("AB", "AC", "BC", "AB.AB", "AB.AC", "AB.CB", "AC.AC", "AC.BC", "AC.AB", "BC.BC", "BC.AC", "BC.BA"), ballots = c("AB", "AC", "BA", "BC", "CA", "CB"), rule = "AV"){
-	uA = utility[1]; uB = utility[2]; uC = utility[3]
-	if(rule == "AV"){
-		out = rbind(
-		# what is the outcome at each pivotal event and ballot
-		c(rep(uA, 2), rep(uB, 2), uA, uB),  # AB
-		c(rep(uA, 2), uA, uC, rep(uC, 2)),  # AC
-		c(uB, uC, rep(uB, 2), rep(uC, 2)),  # BC
-		c(rep(uA, 2), rep(uB, 2), rep((uA + uB)/2, 2)),  # AB.AB
-		c(rep(uA, 2), rep(uC, 2), rep((uA + uC)/2, 2)),  # AB.AC
-		c(rep(uC, 2), rep(uB, 2), rep((uB + uC)/2, 2)),  # AB.CB
-		c(rep(uA, 2), rep((uA + uC)/2, 2), rep(uC, 2)),  # AC.AC
-		c(rep(uB, 2), rep((uB + uC)/2, 2), rep(uC, 2)),  # AC.BC
-		c(rep(uA, 2), rep((uA + uB)/2, 2), rep(uB, 2)),  # AC.AB
-		c(rep((uB + uC)/2, 2), rep(uB, 2), rep(uC, 2)),  # BC.BC
-		c(rep((uA + uC)/2, 2), rep(uA, 2), rep(uC, 2)),  # BC.AC
-		c(rep((uA + uB)/2, 2), rep(uB, 2), rep(uA, 2))   # BC.BA
-		)
-	}else if(rule == "plurality"){
-		piv.events = piv.events[1:3]
-		ballots = c("A", "B", "C")
-		out = rbind(
-			c(uA, uB, (uA + uB)/2),
-			c(uA, (uA + uC)/2, uC),
-			c((uB + uC)/2, uB, uC)
-		)
-	}
-	rownames(out) = piv.events
-	colnames(out) = ballots
-	return(out)
-}
+# outcome_mat_from_utility = function(utility, piv.events = c("AB", "AC", "BC", "AB.AB", "AB.AC", "AB.CB", "AC.AC", "AC.BC", "AC.AB", "BC.BC", "BC.AC", "BC.BA"), ballots = c("AB", "AC", "BA", "BC", "CA", "CB"), rule = "AV"){
+# 	uA = utility[1]; uB = utility[2]; uC = utility[3]
+# 	if(rule == "AV"){
+# 		out = rbind(
+# 		# what is the outcome at each pivotal event and ballot
+# 		c(rep(uA, 2), rep(uB, 2), uA, uB),  # AB
+# 		c(rep(uA, 2), uA, uC, rep(uC, 2)),  # AC
+# 		c(uB, uC, rep(uB, 2), rep(uC, 2)),  # BC
+# 		c(rep(uA, 2), rep(uB, 2), rep((uA + uB)/2, 2)),  # AB.AB
+# 		c(rep(uA, 2), rep(uC, 2), rep((uA + uC)/2, 2)),  # AB.AC
+# 		c(rep(uC, 2), rep(uB, 2), rep((uB + uC)/2, 2)),  # AB.CB
+# 		c(rep(uA, 2), rep((uA + uC)/2, 2), rep(uC, 2)),  # AC.AC
+# 		c(rep(uB, 2), rep((uB + uC)/2, 2), rep(uC, 2)),  # AC.BC
+# 		c(rep(uA, 2), rep((uA + uB)/2, 2), rep(uB, 2)),  # AC.AB
+# 		c(rep((uB + uC)/2, 2), rep(uB, 2), rep(uC, 2)),  # BC.BC
+# 		c(rep((uA + uC)/2, 2), rep(uA, 2), rep(uC, 2)),  # BC.AC
+# 		c(rep((uA + uB)/2, 2), rep(uB, 2), rep(uA, 2))   # BC.BA
+# 		)
+# 	}else if(rule == "plurality"){
+# 		piv.events = piv.events[1:3]
+# 		ballots = c("A", "B", "C")
+# 		out = rbind(
+# 			c(uA, uB, (uA + uB)/2),
+# 			c(uA, (uA + uC)/2, uC),
+# 			c((uB + uC)/2, uB, uC)
+# 		)
+# 	}
+# 	rownames(out) = piv.events
+# 	colnames(out) = ballots
+# 	return(out)
+# }
 
-EU_given_piv_probs_and_utility = function(piv_probs, utility, piv.events = c("AB", "AC", "BC", "AB.AB", "AB.AC", "AB.CB", "AC.AC", "AC.BC", "AC.AB", "BC.BC", "BC.AC", "BC.BA"), ballots = c("AB", "AC", "BA", "BC", "CA", "CB"), rule = "AV"){
-	stopifnot(length(utility) == 3)
-	stopifnot(class(piv_probs) == "list")
-	if(rule == "plurality"){piv.events = piv.events[1:3]}
-	piv.probs = unlist(piv_probs)[piv.events]
-	outcome.mat = outcome_mat_from_utility(utility, piv.events, ballots, rule)
-	as.vector(piv.probs) %*% outcome.mat
-}
+# EU_given_piv_probs_and_utility = function(piv_probs, utility, piv.events = c("AB", "AC", "BC", "AB.AB", "AB.AC", "AB.CB", "AC.AC", "AC.BC", "AC.AB", "BC.BC", "BC.AC", "BC.BA"), ballots = c("AB", "AC", "BA", "BC", "CA", "CB"), rule = "AV"){
+# 	stopifnot(length(utility) == 3)
+# 	stopifnot(class(piv_probs) == "list")
+# 	if(rule == "plurality"){piv.events = piv.events[1:3]}
+# 	piv.probs = unlist(piv_probs)[piv.events]
+# 	outcome.mat = outcome_mat_from_utility(utility, piv.events, ballots, rule)
+# 	as.vector(piv.probs) %*% outcome.mat
+# }
 
 
 # Toy example to check that functions return the same output
 
-v_vec <- c(0.02, 0.08, 0.35, 0.2, 0.175, 0.175, 0, 0, 0)
-s_vec <- rep(85, 4)
-p_probs <- av.pivotal.event.probs.general(v_vec, s_vec)
-u_df <- data.frame(A = 1, B = 0.5, C = 0)
+# v_vec <- c(0.02, 0.08, 0.35, 0.2, 0.175, 0.175, 0, 0, 0)
+# s_vec <- rep(85, 4)
+# p_probs <- av.pivotal.event.probs.general(v_vec, s_vec)
+# u_df <- data.frame(A = 1, B = 0.5, C = 0)
 
-opt_vote(u_df, p_probs)
-EU_given_piv_probs_and_utility(p_probs, c(1, 0.5, 0))
-# Replication successful!
+# opt_vote(u_df, p_probs)
+# EU_given_piv_probs_and_utility(p_probs, c(1, 0.5, 0))
+# # Replication successful!
 
-system.time({opt_vote(u_df, p_probs)})
-system.time({EU_given_piv_probs_and_utility(p_probs, c(1, 0.5, 0))})
+# system.time({opt_vote(u_df, p_probs)})
+# system.time({EU_given_piv_probs_and_utility(p_probs, c(1, 0.5, 0))})
