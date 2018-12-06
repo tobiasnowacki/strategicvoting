8711dc67176d2e031b8a9f0ad9557319eb6ab754
tobiasnowacki <tobynowacki@gmail.com>
2018-12-06 08:59:37 -0800

    Voting paradoxes
    Added last bit of repl

--- code/replication_aus.R
+++ code/replication_aus.R
@@ -67,7 +67,7 @@ const_bp_no_trunc[, 2:10] <- t(apply(const_bp_no_trunc[, 2:10], 1, function(x) x
 
 
 # Set levels of s at which to evaluate.
-s_list <- as.list(c(15, 30, 45, 60, 75, 90, 105))
+s_list <- as.list(c(15, 30, 45, 60, 75, 85, 90))
 
 
 ### PLAYGROUND ### ---
@@ -89,7 +89,10 @@ for(i in 1:nrow(const_bp)){
 save(mega_tau_list, file = here("../output/mega_tau_list.Rdata"))
 # load(here("../output/mega_tau_list.Rdata"))
 
+##
 # From resulting loop, run:
+##
+
 # (1) levels of strategic voting
 prop_list <- lapply(mega_tau_list, function(x) try(sv_prop(x)))
 prop_df <- as.data.frame(do.call(rbind, prop_list))
@@ -120,7 +123,7 @@ aus_freq <- ggplot(prop_df_long, aes(x = s, y = value)) +
 gg_path <- here("../output/figures/australia_sv_freq.pdf")
 ggsave(gg_path, height = 5, width = 4)
 
-# Total strategic incentives
+# (2) Total strategic incentives
 
 prop_df$inc_rcv <- prop_df$RCV_second + prop_df$RCV_third
 prop_df$inc_plur <- prop_df$plur_second
@@ -137,7 +140,7 @@ aus_inc <- ggplot(prop_df, aes(x = inc_plur, y = inc_rcv)) +
 gg_path2 <- here("../output/figures/australia_sv_prop.pdf")
 ggsave(gg_path2, aus_inc, width = 5, height = 5)
 
-# (2) q-q plots
+# (3) q-q plots
 
 qq_mega_list <- lapply(mega_tau_list, function(x) qq_function_two(x, aes_utils_raw))
 qq_mega_df <- as.data.frame(do.call(rbind, qq_mega_list))
@@ -157,11 +160,58 @@ aus_qq <- ggplot(qq_mega_df, aes(x = x, y = y)) +
   xlim(-30, 30) + ylim(-30, 30)
 ggsave(here("../output/figures/australia_sv_qq_trunc.pdf"), aus_qq, width = 6, height = 6)
 
-# (3) occurence of voting paradoxes
+# (4) occurence of voting paradoxes
+### Note that I could do this much faster if I incorporated it into the main loop --
+### that way I wouldn't have to calculate pprobs twice.
 
-# (still have to write code for this one)
+# Non-monotonicity under AV
+nonmon_list <- c()
+s <- 85
+for(i in 1:nrow(const_bp)){
+  print(i)
+  v_vec <- as.numeric(const_bp[i, 2:10] / sum(const_bp[i, 2:10]))
+  pprobs <- av.pivotal.event.probs.general(v_vec, rep(s, 4))
+  nonmon_list[[i]] <- non_monoton(mega_tau_list[[i]], pprobs)
+}
 
-# (4) interdependence
+# Wasted vote under Plurality
+wasted_list <- c()
+for(i in 1:nrow(const_bp)){
+  print(i)
+  v_vec <- as.numeric(const_bp[i, 2:10] / sum(const_bp[i, 2:10]))
+  v_vec_three <- c(v_vec[1] + v_vec[2], v_vec[3] + v_vec[4], v_vec[5] + v_vec[6])
+  pprobs <- plurality.pivotal.probabilities(v_vec_three, s)
+  wasted_list[[i]] <- wasted_vote(mega_tau_list[[i]], pprobs)
+}
+
+paradox_df <- matrix(NA, ncol = 3, nrow = nrow(const_bp))
+for(i in 1:nrow(const_bp)){
+  no_show <- nonmon_list[[i]]$no_show / nonmon_list[[i]]$total
+  nonmon <- (nonmon_list[[i]]$nonmon1 + nonmon_list[[i]]$nonmon2) / nonmon_list[[i]]$total
+  wasted <- wasted_list[[i]]$wasted / wasted_list[[i]]$total
+  paradox_df[i, ] <- c(no_show, nonmon, wasted)
+}
+paradox_df <- as.data.frame(paradox_df)
+names(paradox_df) <- c("no_show", "nonmon", "wasted")
+
+ggplot(paradox_df, aes(x = wasted)) +
+  geom_point(aes(y = no_show)) +
+  geom_point(aes(y = nonmon), colour = "blue") +
+  geom_abline(intercept = 0, slope = 1) +
+  xlim(0, 0.5) + ylim(0, 0.15)
+
+# Check if Andy's function yields the same result...
+# v_vec <- const_bp_no_trunc[4, 2:10] / sum(const_bp_no_trunc[1, 4:10])
+# names(aes_utils_raw) <- c("A", "B", "C")
+# test <- sv(U = aes_utils_raw, s = 85, v.vec = as.numeric(v_vec)[1:6], rule = "AV")
+# 
+# test_toby_p <- av.pivotal.event.probs.general(as.numeric(v_vec), rep(85, 4))
+# non_monoton(mega_tau_list[[4]], test_toby_p)
+# no_show_non_mon_from_sv_object(test)
+# Yes! (without truncated ballots, that is.)
+
+
+# (5) interdependence
 
 # Set s = 80
 mega_tau_fixed_s <- lapply(mega_tau_list, function(x) x[x$s == 75, ])
@@ -193,131 +243,3 @@ l0_plot <- ggplot(inter_df_full, aes(x = lambda, group = const)) +
   geom_line(aes(y = L0PLUR), colour = "orange", alpha = 0.25) +
   theme_bw()
 
-### OLD STUFF ----
-
-
-# LEVELS OF STRATEGIC VOTING.
-# TODO: Consider re-writing return_sv_prop such that it works off the return_sv_tau DF
-# Ideal: run one loop over all s and constituencies with return_sv_tau; then create plots off that
-
-
-
-
-
-# # Run loop over all constituencies.
-# const_opt_dist <- list()
-# for(i in 1:nrow(const_bp_no_trunc)){
-# 	print(i)
-# 	prop <- return_sv_prop(const_bp_no_trunc[i, 2:10], aes_utils[, 1:3], s_list)
-# 	prop[, 1:3] <- as.data.frame(t(apply(prop[, 1:3], 1, function(x) x / sum(x))))
-# 	prop$const <- const_bp_no_trunc[i, 1]
-# 	const_opt_dist[[i]] <- prop
-# }
-# 
-# const_opt_dist_df_wide <- do.call(rbind, const_opt_dist)
-# const_opt_dist_df <- melt(const_opt_dist_df_wide[, c("second", "third", "plurality_second", "const", "s")], 
-# 							id.vars = c("const", "s"))
-# 
-# # Get means.
-# const_opt_dist_df_agg <- as.data.frame(const_opt_dist_df %>% 
-# 							group_by(variable, s) %>% 
-# 							summarize(mean(value)))
-# names(const_opt_dist_df_agg)[3] <- "value"
-# 
-# # Plot. (SI over levels of s, by SI type)
-# aus_freq <- ggplot(const_opt_dist_df, aes(x = s, y = value)) +
-# 	geom_line(aes(colour = variable, group = interaction(const, variable)), alpha = 0.3) +
-# 	geom_line(data = const_opt_dist_df_agg, aes(colour = variable, group = variable, x = s, y = value),
-# 		size = 3) + 
-# 	labs(x = "Information (s)", 
-# 		y = "Proportion of voters in AES casting ballot type",
-# 		colour = "Sincere pref. as first on ballot") +
-# 	theme_bw() +
-# 	theme(legend.position = "bottom")
-# gg_path <- here("output/figures/australia_sv_freq.pdf")
-# ggsave(gg_path)
-
-# Next, let's have a distribution of cases according to whether they are more amenable to SV in RCV or Plurality.
-# head(const_opt_dist_df_wide)
-
-# ## Table: by constituency
-# const_opt_dist_df_wide$inc_rcv <- const_opt_dist_df_wide[, 2] + const_opt_dist_df_wide[, 3]
-# const_opt_dist_df_wide$inc_plur <- const_opt_dist_df_wide$plurality_second
-# 
-# # Plot. (Total SI, for a given level of s)
-# aus_prop <- ggplot(const_opt_dist_df_wide, aes(x = inc_plur, y = inc_rcv)) +
-# 	geom_point() +
-# 	geom_abline(slope = 1, intercept = 0) +
-# 	facet_wrap(~ s) +
-# 	theme_bw() +
-# 	scale_x_continuous(limits = c(0, 0.7), expand = c(0, 0)) +
-# 	scale_y_continuous(limits = c(0, 0.7), expand = c(0, 0)) +
-# 	labs(x = "Proportion of AES respondents with positive SI under Plurality",
-# 		y = "Proportion of AES respondents with positive SI under RCV")
-# gg_path2 <- here("output/figures/australia_sv_prop.pdf")
-# ggsave(gg_path2)
-# 
-
-## QQ-PLOT
-# 
-# # Create dataframe of qq-plot coordinates, by constituency and by s
-# qq_list <- lapply(s_list, function(x) qq_function(const_bp_no_trunc, aes_utils[, 1:3], x))
-# qq_df <- do.call(rbind, qq_list)
-# qq_df$s <- unlist(rep(as.vector(s_list), each = nrow(aes_utils) * nrow(const_bp_no_trunc)))
-# 
-# # Also get coordinates for qq-plot aggregated over constituencies, by s
-# big_qq_list <- lapply(qq_list, function(z) as.data.frame(qqplot(x = z$x, y = z$y, plot.it = FALSE)))
-# big_qq_df <- do.call(rbind, big_qq_list)
-# big_qq_df$s <- rep(unlist(s_list), each = nrow(aes_utils) * nrow(const_bp_no_trunc))
-# 
-# # Plot.
-# qq_plot_faceted <- ggplot(qq_df) +
-# 	geom_line(aes(x = x, y = y, group = const), alpha = 0.1) +
-# 	geom_line(data = big_qq_df, aes(x = x, y = y), colour = "red", lwd = 2) + 
-# 	geom_abline(intercept = 0, slope = 1, linetype = "dotted", colour = "blue") +
-# 	theme_bw()  + 
-# 	facet_wrap(vars(s))
-# ggsave(here("../output/figures/australia_sv_qq.pdf"), qq_plot_faceted)
-
-## OCCURRENCE OF VOTING PARADOXES
-
-# todo: write functions (wasted vote; non-monotonicity)
-
-
-## STRATEGIC INTERDEPENDENCE
-
-# Run the actual loop across constituencies
-inter_df <- list()
-lambda_list <- as.list(seq(0, 0.5, 0.02))
-for(i in 1:nrow(const_bp)){
-	print(i)
-	v_vec <- as.numeric(const_bp_no_trunc[i, 2:10]) / sum(as.numeric(const_bp_no_trunc[i, 2:10]))
-	tau <- return_sv_tau(v_vec, aes_utils[, 1:3], list(80))
-	const_props <- level_two_props(v_vec, lambda_list, aes_utils[, 1:3], tau, list(80))
-	const_props$const <- const_bp[i, 1]
-	inter_df[[i]] <- const_props
-}
-
-inter_df_full <- do.call(rbind, inter_df)
-
-names(inter_df_full)[1:4] <- c("L1RCV", "L0RCV", "L1PLUR", "L0PLUR")
-
-lvl1_diff <- ggplot(inter_df_full, aes(x = lambda)) +
-	geom_line(aes(y = L1RCV, group = const), colour = "blue", alpha = 0.2) +
-	geom_line(aes(y = L1PLUR, group = const), colour = "orange", alpha = 0.2) +
-	theme_bw() +
-	labs(x = expression(lambda), y = paste(expression(delta), "Level 2 Strat. Vote - Level 1 Strat. Vote"))
-ggsave(here("../output/figures/level1_diff.pdf"), lvl1_diff)
-
-
-lvl0_diff <- ggplot(inter_df_full, aes(x = lambda)) +
-	geom_line(aes(y = L0RCV, group = const), colour = "blue", alpha = 0.2) +
-	geom_line(aes(y = L0PLUR, group = const), colour = "orange", alpha = 0.2) +
-	theme_bw() +
-	labs(x = expression(lambda), y = paste(expression(delta), "Level 2 Strat. Vote - Sincere Vote"))
-ggsave(here("../output/figures/level0_diff.pdf"))
-
-# compare to original
-return_sv_prop(c(v_vec_init_weighted, 0, 0, 0), aes_utils[, 1:3], s_list)
-
-# Loop over constituencies and put into mega-dataframe for plotting.
--- code/replication_cses.R
+++ code/replication_cses.R
@@ -42,7 +42,6 @@ load(here("../output/cses_big_list_2.RData"))
 ### ANALYSIS
 ###
 
-# Create list with v_vecs from CSES utility dfs:
 remove_nas <- function(x){
   mat <- cbind(x$U, x$weights)
   mat <- na.omit(mat)
@@ -59,9 +58,9 @@ create_v_vec <- function(x){
   return(sin_vec / sum(sin_vec))
 }
 
+# Create list with v_vecs from CSES utility dfs:
 big_list_na_omit <- lapply(big_list, function(x) remove_nas(x))
 sin_vote_list <- lapply(big_list_na_omit, function(x) sincere.vote.mat.from.U(x$U, rule = "AV"))
-
 v_vec_list <- list()
 for(i in 1:length(sin_vote_list)){
   weights <- big_list_na_omit[[i]]$weights
@@ -69,8 +68,10 @@ for(i in 1:length(sin_vote_list)){
   big_list_na_omit[[i]]$v_vec <- as.numeric(v_vec)
 }
 
+# Set list of s values
 s_list <- as.list(c(15, 30, 45, 60, 75, 85, 90))
 
+# Drop NA cases
 names(big_list)[[39]]
 names(big_list)[[145]]
 big_list_na_omit[[39]] <- NULL
@@ -84,7 +85,7 @@ big_list_na_omit[[144]] <- NULL
 #   sv_list[[i]] <- return_sv_tau(c(big_list_na_omit[[i]]$v_vec, 0, 0, 0), big_list_na_omit[[i]]$U, s_list)
 # }
 
-# Use Andy's function to get us started and convert into my dataframe
+# Use Andy's function to create list of sv objects for each case
 sv_list <- list()
 for(i in 1:length(big_list_na_omit)){
   print(i)
@@ -127,7 +128,9 @@ save(file = here("../output/sv_list.Rdata"), sv_list)
 # load(here("../output/sv_list.Rdata"))
 
 
-# Proportion of optimal strategic votes
+## Additional analysis off the sv_obj cases
+
+# (1) Proportion of optimal strategic votes
 prop_list <- list()
 for(i in 1:length(sv_list)){
   print(i)
@@ -147,7 +150,6 @@ prop_df_agg <- as.data.frame(prop_df_long %>%
                                summarize(mean(value)))
 names(prop_df_agg)[3] <- "value"
 
-
 cses_prop <- ggplot(prop_df_long, aes(x = s, y = value)) +
   geom_line(aes(colour = variable, group = interaction(case, variable)), alpha = 0.05) +
   geom_line(data = prop_df_agg, aes(colour = variable, group = variable, x = s, y = value),
@@ -162,7 +164,7 @@ cses_prop <- ggplot(prop_df_long, aes(x = s, y = value)) +
   theme(legend.position = "bottom", legend.direction = "vertical")
 ggsave(here("../output/figures/cses_freq.pdf"), cses_prop, height = 5, width = 4)
 
-# Distribution of incentives (in scatterplot)
+# (2) Distribution of incentives (in scatterplot)
 prop_df$inc_rcv <- prop_df$rcv_second + prop_df$rcv_third
 prop_df$inc_plur <- prop_df$plur_second
 
@@ -198,7 +200,7 @@ ggsave(here("../output/figures/cses_prop.pdf"), cses_inc, width = 5, height = 5)
 #   scale_x_continuous(limits = c(0, 0.7), expand = c(0, 0)) +
 #   scale_y_continuous(limits = c(0, 0.7), expand = c(0, 0))
 
-## QQ-Plots
+## (3) QQ-Plots
 
 qq_mega_list <- list()
 for(i in 1:length(sv_list)){
@@ -229,14 +231,56 @@ cses_qq <- ggplot(qq_mega_df, aes(x = x, y = y)) +
 ggsave(here("../output/figures/cses_qq.pdf"), cses_qq, width = 6, height = 6)
 
 
-# Voting paradoxes
+# (4) Voting paradoxes
+
+# Look at AUS case for successful code. Functions should work either way
+
+s <- 85
+paradox_df <- matrix(NA, ncol = 3, nrow = length(big_list_na_omit))
+for(i in 1:length(big_list_na_omit)){
+  print(i)
+  v_vec <- big_list_na_omit[[i]]$v_vec
+  pprobs_rcv <- av.pivotal.event.probs.general(c(v_vec, 0, 0, 0), rep(s, 4))
+  par_rcv <- non_monoton(sv_list[[i]][sv_list[[i]]$s == s, ], pprobs_rcv, weights = big_list_na_omit[[i]]$weights)
+  v_vec_plur <- c(v_vec[1] + v_vec[2], v_vec[3] + v_vec[4], v_vec[5] + v_vec[6])
+  pprobs_plur <- plurality.pivotal.probabilities(v_vec_plur, s)
+  par_plur <- wasted_vote(sv_list[[i]][sv_list[[i]]$s == s, ], pprobs_plur, big_list_na_omit[[i]]$weights)
+  
+  no_show <- par_rcv$no_show / par_rcv$total
+  nonmon <- (par_rcv$nonmon1 + par_rcv$nonmon2) / par_rcv$total
+  wasted <- par_plur$wasted / par_plur$total
+  paradox_df[i, ] <- c(no_show, nonmon, wasted)
+}
+
+test <- sv(big_list_na_omit[[1]]$U, big_list_na_omit[[1]]$weights, s = 85)
+plurality_wasted_vote_from_sv_object(test)
+v_vec <- big_list_na_omit[[1]]$v_vec
+v_vec_plur <- c(v_vec[1] + v_vec[2], v_vec[3] + v_vec[4], v_vec[5] + v_vec[6])
+test_props <- plurality.pivotal.probabilities(v_vec_plur, 85)
+wasted_vote(sv_list[[1]][sv_list[[1]]$s == 85,], test_props, big_list_na_omit[[1]]$weights)
 
 
+paradox_df <- as.data.frame(paradox_df)
+names(paradox_df) <- c("no_show", "nonmon", "wasted")
 
-# Interdependence
+ggplot(paradox_df, aes(x = wasted)) +
+  geom_point(aes(y = no_show, colour = "No-show"), size = 1.5, alpha = 0.2) +
+  geom_point(aes(y = nonmon, colour = "Non-mon"), size = 1.5, alpha = 0.2) +
+  geom_abline(intercept = 0, slope = 1, lty = "dotted") +
+  geom_smooth(method = "loess", aes(y = no_show), colour = "blue") +
+  geom_smooth(method = "loess", aes(y = nonmon), colour = "red") +
+  xlim(0, 0.4) + ylim(0, 0.4) +
+  labs(x = "Pr(Wasted Vote, Plurality)", y = "Pr(Voting Paradox, RCV)") +
+  scale_colour_manual(breaks = c("No-show", "Non-mon"), values = c("No-show" = "blue", "Non-mon" = "red")) +
+  theme_bw() +
+  theme(legend.position = "bottom", legend.direction = "horizontal")
+
+
+# (5) Interdependence
 
 # Isolate DF for just one instance of s
-sv_list_fixed_s <- lapply(sv_list, function(x) x[x$s == 75, ])
+s <- 85
+sv_list_fixed_s <- lapply(sv_list, function(x) x[x$s == s, ])
 
 lambda_list <- as.list(seq(0, 0.5, 0.05))
 
@@ -245,7 +289,7 @@ for(i in 1:length(big_list_na_omit)){
   print(i)
   df <- big_list_na_omit[[i]]
   sv_item <- sv_list_fixed_s[[i]]
-  out <- level_two_props_cses(c(df$v_vec, 0, 0, 0), lambda_list, df$U, sv_item, 75, df$weights)
+  out <- level_two_props_cses(c(df$v_vec, 0, 0, 0), lambda_list, df$U, sv_item, s, df$weights)
   out$case <- names(big_list_na_omit)[[i]]
   inter_list[[i]] <- out
 }
