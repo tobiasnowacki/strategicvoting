5f238a081762002bb1697ebe25c54177227a3bc7
tobiasnowacki <tobynowacki@gmail.com>
2019-05-19 17:18:01 -0700

    redraft

--- code/paper_code_eqm.r
+++ code/paper_code_eqm.r
@@ -10,7 +10,7 @@
 ##
 
 # Load packages
-requiredPackages <- c("here", "ggplot2", "ggtern", "dplyr", "purrr", "tidyr", "lmtest", "sandwich", "plm", "extrafont", "RColorBrewer", "boot", "svMisc", "ggtern")
+requiredPackages <- c("here", "ggplot2", "ggtern", "dplyr", "purrr", "tidyr", "lmtest", "sandwich", "plm", "extrafont", "RColorBrewer", "boot", "svMisc", "ggtern", "reldist")
 ipak <- function(pkg){
         new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
         if (length(new.pkg))
@@ -79,8 +79,8 @@ big_list_na_omit[[144]] <- NULL
 
 # Create main sv_list object (this could be done much more elegantly with a function)
 sv_list <- list()
-country_weight <- c()
 n <- length(big_list_na_omit)
+country_weight <- matrix(nrow = n, ncol = 2)
 for(i in 1:n){
   progress(i)
   if(i == n) cat("Done! \n")
@@ -95,7 +95,8 @@ for(i in 1:n){
   df$m <- vap$Freq[vap$cntry == df$country[1]]
   df$weight_rep <- df$weight * (df$VAP / (df$weight_sum * df$m))
   #df <- apply(df, 2, as.numeric)
-  country_weight[i] <- df$VAP[1] / df$m[1]
+  country_weight[i, 1] <- names(big_list_na_omit)[[i]]
+  country_weight[i, 2] <- df$VAP[1] / df$m[1]
   sv_list[[i]] <- df
 }
 
@@ -196,8 +197,8 @@ for(prec in c(1, 4, 6)){
   big_plur_vec[[prec]] <- plur_vec
 }
 
-save.image(here("output/intermediate2.Rdata"))
-# load(here("output/intermediate2.RData"))
+# save.image(here("output/intermediate2.Rdata"))
+load(here("output/intermediate2.RData"))
 
 big_rcv_sum <- do.call(rbind, big_rcv_sum)
 big_plur_sum <- do.call(rbind, big_plur_sum)
@@ -205,20 +206,12 @@ big_plur_sum <- do.call(rbind, big_plur_sum)
 
 ## QUANTITIES OF INTEREST
 
-## CONJECTURE 1
+## CONJECTURE 1a
 # Summed pivotal probabilities
 rcv_summary <- big_rcv_sum %>% group_by(case, s, iter) %>% summarise_at(vars(AB:BCp), first) 
 rcv_summary$rcv_sum <- rowSums(rcv_summary[, 4:16])
 rcv_summary$plur_sum <- rowSums(rcv_summary[, 7:19])
 rcv_summary <- rcv_summary %>% mutate(diff = plur_sum - rcv_sum)
-
-ggplot(rcv_summary, aes(iter, diff)) +
-  geom_line(aes(group = interaction(case, s)), alpha = 0.05) +
-  geom_hline(yintercept = 0, lty = "dotted") +
-  facet_wrap(. ~ s) +
-  theme_sv() +
-  labs(x = "Iteration", y = "Pr(Pivotal | Plur) - Pr(Pivotal | RCV)")
-ggsave(here("output/figures/conj1_rcv.pdf"), device = cairo_pdf, height = 3, width = 6)
 
 # Do the same thing for plurality
 plur_summary <- big_plur_sum %>% group_by(case, s, iter) %>% summarise_at(vars(AB:BCp), first) 
@@ -226,42 +219,103 @@ plur_summary$rcv_sum <- rowSums(plur_summary[, 4:16])
 plur_summary$plur_sum <- rowSums(plur_summary[, 7:19])
 plur_summary <- plur_summary %>% mutate(diff = plur_sum - rcv_sum)
 
-ggplot(plur_summary, aes(iter, diff)) +
-  geom_line(aes(group = interaction(case, s)), alpha = 0.05) +
-  geom_hline(yintercept = 0, lty = "dotted") +
-  facet_wrap(. ~ s) +
-  theme_sv() +
-  labs(x = "Iteration", y = "Pr(Pivotal | Plur) - Pr(Pivotal | RCV)")
-ggsave(here("output/figures/conj1_plur.pdf"), device = cairo_pdf, height = 3, width = 6)
-
-## CONJECTURE 2
-
+## CONJECTURES 1b and 2
 cmat_plur <- matrix(c(1, 2, 3, 2, 1, 3, 1, 3, 2, 3, 1, 2, 2, 3, 1, 3, 2, 1), byrow = T, ncol = 3)
 
 cmat_rcv <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 1, 3, 7, 8, 9, 4, 5, 6, 10, 12, 11, 1, 3, 2, 4, 6, 5, 10, 11, 12, 7, 8, 9, 3, 1, 2, 10, 11, 12, 4, 6, 5, 7, 9, 8, 2, 3, 1, 7, 9, 8, 10, 12, 11, 4, 5, 6, 3, 2, 1, 10, 12, 11, 7, 9, 8, 4, 6, 5), byrow = T, ncol = 12)
 
 cmat_u <- matrix(c(1, 2, 3, 1, 3, 2, 2, 1, 3, 2, 3, 1, 3, 1, 2, 3, 2, 1), byrow = T, ncol = 3)
 
-testdf <- big_rcv_sum %>% filter(s == 85)
-ordered_u_probs <- t(apply(testdf, 1, rearrange))
+# re-order RCV DF
+rcvdf85 <- big_rcv_sum %>% filter(s == 85)
+ordered_u_probs <- t(apply(rcvdf85, 1, rearrange))
 ordered_u_probs <- apply(ordered_u_probs, 2, as.numeric)
 
-testdf2 <- as.data.frame(ordered_u_probs)
-testdf2 <- testdf2 %>% mutate(ben_p = BCpo * ((uB - uC) / 2), cost_p = ABpo * (uA - uB) + ACpo * ((uB - uC)/2),
-ben_rcv = AB_CBo * (uB - uC) + BC_BCo * ((uB - uC)/2) + BC_ACo * ((uA - uC)/2), cost_rcv = ABo * (uA - uB) + AB_ABo * (uA - uB) + AB_ACo * (uA - uC) + AC_ACo * ((uA - uC) / 2) + AC_ABo * ((uA - uB) / 2) + AC_BCo * ((uB - uC)/2) + BC_BAo * ((uA - uB)/2))
+rcvdf852 <- as.data.frame(ordered_u_probs)
+rcvdf852 <- rcvdf852 %>% mutate(
+  ben_rcv2 = AB_CBo * (uB - uC) + BC_BCo * ((uB - uC)/2) + BC_ACo * ((uA - uC)/2), 
+  cost_rcv2 = ABo * (uA - uB) + AB_ABo * (uA - uB) + AB_ACo * (uA - uC) + AC_ACo * ((uA - uC) / 2) + AC_ABo * ((uA - uB) / 2) + AC_BCo * ((uB - uC)/2) + BC_BAo * ((uA - uB)/2),
+  ben_rcv3 = AB_CBo * ((uB - uC) / 2) + BC_BAo * ((uA - uB) / 2),
+  cost_rcv3 = ACo * (uA - uC) + BCo * (uB - uC) + AB_ABo * ((uA - uB) / 2) + AB_ACo * (uA - uC)/2 + AC_ACo * (uA - uC)/2 + AC_ABo * (uA - uB) + BC_BCo * (uC - uB)/2 + BC_ACo * (uA - uC)/2,
+  pprob2 = AB_CBo + BC_BCo + BC_ACo, 
+  pprob3 = AB_CBo + BC_BAo)
 
 # compute correlations
-testdf3 <- cbind(testdf, testdf2)
-testdf3 <- testdf3 %>% group_by(case, iter) %>% summarise(corr_plur = cor(ben_p, cost_p), corr_rcv = cor(ben_rcv, cost_rcv))
+rcvdf853 <- cbind(rcvdf85, rcvdf852)
+rcvdf853 <- rcvdf853 %>% group_by(case, iter) %>% summarise(corr_rcv2 = cor(ben_rcv2, cost_rcv2), corr_rcv3 = cor(ben_rcv3, cost_rcv3),pprob2 = weighted.mean(pprob2, weight = w), pprob3 = weighted.mean(pprob3, weight = w), path = "IRV")
 
-ggplot(testdf3, aes(iter, corr_rcv)) +
-  geom_line(aes(group = case), alpha = 0.05) +
-  geom_line(aes(y = corr_plur, group = case), colour = "blue", alpha = 0.05) +
-  geom_hline(yintercept = 0, lty = "dotted") +
-  theme_sv() +
-  labs(x = "Iteration", y = "Correlation")
-ggsave(here("output/figures/conj2_rcv.pdf"), device = cairo_pdf, height = 3, width = 6)
+# Tidy DF for plotting
+rcvdf854 <- rcvdf853 %>% gather(key = "quantity", value = "value", corr_rcv2:pprob3) 
+rcvdf854 <- rcvdf854 %>% mutate(type = ifelse(quantity %in% c("corr_rcv2", "pprob2"), "IRV_second", "IRV_third"), quantity = ifelse(quantity %in% c("corr_rcv2", "corr_rcv3"), "correlation", "pprob"))
+rcvdf854 <- spread(rcvdf854, key = quantity, value = value)
 
+# Re-order plurality DF
+plurdf85 <- big_plur_sum %>% filter(s == 85)
+ordered_u_probs_plur <- t(apply(plurdf85, 1, rearrange))
+ordered_u_probs_plur <- apply(ordered_u_probs_plur, 2, as.numeric)
+
+# Tidy DF for plotting
+plurdf852 <- as.data.frame(ordered_u_probs_plur)
+plurdf852 <- plurdf852 %>% mutate(ben_p = BCpo * ((uB - uC) / 2), cost_p = ABpo * (uA - uB) + ACpo * ((uB - uC)/2), pprob_plur = BCpo
+)
+
+# compute correlations
+plurdf853 <- cbind(plurdf85, plurdf852)
+plurdf853 <- plurdf853 %>% group_by(case, iter) %>% summarise(correlation = cor(ben_p, cost_p), pprob = weighted.mean(pprob_plur, weight = w), type = "Plurality", path = "Plurality")
+
+
+# Bind DF together and tidy
+weightdf <- as.data.frame(country_weight) 
+names(weightdf) <- c("case", "ctryweight")
+weightdf$ctryweight <- as.numeric(as.character(weightdf$ctryweight))
+
+conjdf <- rbind(plurdf853, rcvdf854) %>% left_join(weightdf)
+conjdf_quant <- conjdf %>% group_by(iter, type) %>% summarise(
+  corr_q025 = wtd.quantile(correlation, q = 0.025, weight = ctryweight),
+  corr_q25 = wtd.quantile(correlation, q = 0.25, weight = ctryweight),
+  corr_q50 = wtd.quantile(correlation, q = 0.5, weight = ctryweight),
+  corr_q75 = wtd.quantile(correlation, q = 0.75, weight = ctryweight),
+  corr_q975 = wtd.quantile(correlation, q = 0.975, weight = ctryweight),
+  corr_mean = wtd.mean(correlation, weight = ctryweight),
+  pprob_q025 = wtd.quantile(pprob, q = 0.025, weight = ctryweight),
+  pprob_q25 = wtd.quantile(pprob, q = 0.25, weight = ctryweight),
+  pprob_q50 = wtd.quantile(pprob, q = 0.5, weight = ctryweight),
+  pprob_q75 = wtd.quantile(pprob, q = 0.75, weight = ctryweight),
+  pprob_q975 = wtd.quantile(pprob, q = 0.975, weight = ctryweight),
+  pprob_mean = wtd.mean(pprob, weight = ctryweight))
+
+
+
+# Pprobs plot (raw)
+ggplot(conjdf, aes(x = iter)) +
+geom_line(aes(y = pprob, group = interaction(case, type), colour = type), alpha = 0.1) +
+geom_line(data = conjdf_quant, aes(x = iter, y = pprob_mean, group = type, colour = type), lwd = 2) + 
+geom_hline(yintercept = 0, lty = "dashed") +
+labs(x = "Iteration", y = "Pivotal Probabilities") +
+theme_sv() 
+ggsave(here("output/figures/conj1.pdf"), device = cairo_pdf)
+
+# Pprobs plot (quantiles)
+ggplot(conjdf_quant, aes(x = iter)) +
+geom_line(aes(y = pprob_q50, group = type, colour = type), alpha = 1) +
+geom_ribbon(aes(ymin = pprob_q25, ymax = pprob_q75, group = type, fill = type), alpha = 0.25) + 
+geom_hline(yintercept = 0, lty = "dashed") +
+theme_sv() 
+
+# Correlations plot
+ggplot(conjdf, aes(x = iter)) +
+geom_line(aes(y = correlation, group = interaction(case, type), colour = type), alpha = 0.05) +
+geom_line(data = conjdf_quant, aes(x = iter, y = corr_mean, group = type, colour = type), lwd = 2) + 
+geom_hline(yintercept = 0, lty = "dashed") +
+theme_sv() 
+ggsave(here("output/figures/conj2.pdf"), device = cairo_pdf)
+
+# Correlations plot (quantiles)
+ggplot(conjdf_quant, aes(x = iter)) +
+geom_line(aes(y = corr_q50, group = type, colour = type), alpha = 1) +
+geom_ribbon(aes(ymin = corr_q25, ymax = corr_q75, group = type, fill = type), alpha = 0.25) + 
+geom_hline(yintercept = 0, lty = "dashed") +
+theme_sv() 
 
 ## SUMMARY OF INCENTIVES
 # Prevalence, Magnitude, EB
