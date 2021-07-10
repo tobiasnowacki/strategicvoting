7b726cf774bbfa573c75dd640f46dc9d9b82a62f
tobiasnowacki <tobynowacki@gmail.com>
2021-07-09 17:31:15 -0700

    new bash script for 4 parties

---
+++ server_four.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --time=2:00:00
+#SBATCH --mem=266G
+#SBATCH --partition=bigmem
+#SBATCH --ntasks-per-node=4
+#SBATCH --job-name="servercode"
+#SBATCH --error=rep4.stderr
+#SBATCH --output=rep4.out
+#SBATCH --mail-user=toby.nowacki@gmail.com
+#SBATCH --mail-type=ALL
+
+echo $1
+echo $2
+module load R/4.1
+module load cairo
+Rscript code/new_iter_algo_four.R $1 $2
