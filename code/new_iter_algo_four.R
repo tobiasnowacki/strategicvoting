e959d1e03db78ac70f4e8c6ab34e4f8b49c940b9
tobiasnowacki <tobynowacki@gmail.com>
2021-07-09 17:29:45 -0700

    four-party implementation

---
+++ code/new_iter_algo_four.R
@@ -0,0 +1,75 @@
+# LOAD DEPENDENCIES
+library(tidyverse)
+library(pivotprobs)
+library(gtools)
+library(doParallel)
+
+# Load functions
+source("code/utils/sv_iter_four.R")
+# Load data
+load("output/big_list_4_parties.RData")
+vap <- read.csv("data/case_vap.csv", sep = "") # voting age pop.
+cat("Data imported. \n")
+source("code/prep_cses.R") # data prep
+
+# which(names(big_list_na_omit) == "SWE_2014")
+
+# SET PARAMETERS
+s_val <- 85
+lambda_val <- 0.05
+max_iter_val <- 100 # no of iterations
+which_cases <- 1:160 # which cases?
+
+# Override previous .txt file
+close(file("clusterlog_four.txt", open = "w"))
+
+# MULTIPLE CORES
+clno <- detectCores()
+cl <- makeCluster(clno, outfile = "clusterlog_four.txt")
+registerDoParallel(cl)
+result_list <- list()
+# parallelise
+cases_converge <- foreach(
+    j = which_cases,
+    .packages = c(
+        "gtools", "stringr", "tidyverse", "pivotprobs",
+        "questionr"
+    )
+) %dopar% {
+    case <- big_list_na_omit[[j]]
+    inner_list <- list()
+    cat(paste0("Starting case ", j, " out of ", max(which_cases), ".", "\n"))
+    inner_list$rcv <- sv_iter(
+        U = case$U,
+        s = s_val,
+        rule = "AV",
+        weights = case$weights,
+        lambda = lambda_val,
+        max.iterations = max_iter_val,
+        noisy = FALSE
+    )
+    inner_list$plur <- sv_iter(
+        U = case$U,
+        s = 85,
+        rule = "plurality",
+        weights = case$weights,
+        lambda = lambda_val,
+        max.iterations = max_iter_val,
+        noisy = FALSE
+    )
+
+    filepath <- paste0(
+        "output/files/", 
+        l_choice, 
+        "/", 
+        s_val, 
+        "_", 
+        names(big_list_na_omit)[j], "_iterout_fourparties.Rdata"
+    )
+    save(inner_list, file = filepath)
+    cat(paste0("Done with case ", j, " out of ", max(which_cases), ".", "\n"))
+    inner_list
+}
+stopCluster(cl)
+# Save results
+cat("Script complete.")
---
+++ code/utils/sv_four.R
@@ -0,0 +1,132 @@
+#### sv
+## Adapted for four candidates
+# function to get strategic voting info given
+# utility matrix, weights, and ingredients of (Dirichlet) belief
+library(pivotprobs)
+
+source("code/utils/new_sv_helpers.R")
+
+sv_four <- function(U,
+                    weights = NULL,
+                    v.vec = NULL,
+                    s,
+                    rule = "plurality",
+                    V0 = NULL,
+                    ae_pack = TRUE) {
+    stopifnot(!is.null(colnames(U)))
+    candidates <- sort(colnames(U))
+    U <- U[, candidates]
+
+    stopifnot(rule %in% c("plurality", "AV"))
+    stopifnot(ncol(U) == 4)
+
+    if (is.null(weights)) {
+        weights <- rep(1, nrow(U))
+    }
+
+    # get sincere vote mat (V0)
+    if (is.null(V0)) {
+        V0 <- sincere.vote.mat.from.U(U, rule = rule, candidates = candidates)
+    }
+
+    if (is.null(v.vec)) {
+        v.vec <- ballot.props.from.vote.mat.and.weights(V0, weights = weights)
+    }
+
+    if (rule %in% c("AV")) {
+        ballots <- apply(
+            permutations(
+                n = length(candidates),
+                r = length(candidates),
+                v = candidates,
+                repeats.allowed = FALSE
+            ),
+            1,
+            paste,
+            collapse = ""
+        )
+    } else {
+        ballots <- candidates
+    }
+
+    # IRV pivprobs
+    if (rule == "AV") {
+        # Step 1: Simulate results
+        mc_sims <- simulate_ordinal_results_from_dirichlet(
+            k = 4,
+            n = 100000,
+            alpha = v.vec * s
+        )
+        # Step 2: Calculate pprobs
+        pps <- mc_sims %>%
+            irv_pivot_probs_four_cands(n = 1)
+        # Step 3: Get pmat
+        P.mat <- pps %>%
+            combine_P_matrices()
+        P.mat <- P.mat[, -25] # delete abstention
+    } else if (rule == "plurality") {
+        pps <- plurality_election(k = 4, n = 1) %>%
+            election_event_probs(method = "ev", alpha = (v.vec * s))
+        P.mat <- pps %>% combine_P_matrices()
+        P.mat <- P.mat[, -5]
+        pps <- map(pps, "integral")
+    }
+
+    # Normalize (this is old code)
+    probability.pivotal <- sum(P.mat[, 1])
+    normalized.P.mat <- P.mat
+
+    # Set up ballot matrices
+    ballot.prop.mat <- V1.ballot.prop.mat <- V0.ballot.prop.mat <- NULL
+    eu.by.ballot <- as.matrix(U) %*% normalized.P.mat
+    colnames(eu.by.ballot) <- ballots
+    rownames(P.mat) <- candidates
+    colnames(P.mat) <- ballots
+    colnames(eu.by.ballot) <- ballots
+    sincere_pref_mat <- sincere_pref_mat_from_U(
+        U,
+        rule = rule, 
+        candidates = candidates
+    )
+    V.mat <- ballot_mat_from_eu_mat(
+        eu.by.ballot,
+        break_ties_with_sincerity = TRUE,
+        sincere_mat = sincere_pref_mat
+    )
+
+    # determine optimal vote
+    opt.votes.strategic <- optimal.vote.from.V.mat(V.mat)
+    opt.votes.sincere <- optimal.vote.from.V.mat(V0)
+
+    # calculate tau
+    tau <- get.tau.from.eu.by.ballot.and.V0(
+        eu.by.ballot,
+        V0,
+        sincere_mat = sincere_pref_mat
+    )
+
+      # get best response v.vec
+    br.v.vec <- ballot.props.from.vote.mat.and.weights(
+      V.mat,
+      weights = weights
+    )
+      
+    # now output
+    return(
+        list(
+           opt.votes.strategic = opt.votes.strategic,
+           opt.votes.sincere = opt.votes.sincere, 
+           piv.probs = pps, 
+           tau = tau, 
+           weights = weights, 
+           V.mat = V.mat, 
+           V0 = V0, 
+           eu.mat = eu.by.ballot, 
+           best.response.v.vec = br.v.vec,
+           v.vec.before = v.vec,
+           p.mat <- P.mat
+        )
+    )
+}
+
+
